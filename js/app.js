cde3ec7859d16c407a70ec70218f1d1bfbfa7d9c
YulianaAmaya96 <34106480+YulianaAmaya96@users.noreply.github.com>
2018-01-25 11:27:55 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
+var  items=[], ciu, tip;
+
+function mostrarTodos(event){
+  event.preventDefault();
+  $(".colContenido").html(items);
+}
+
 function envio_de_datos(){
     var ciudad = $('#selectCiudad').val();
     var tipo = $('#selectTipo').val();
     var rango = $('#rangoPrecio').val();
-    var filtro= 'ciudad=' + ciudad +'&tipo=' + tipo + '&precio=' + rango;
+    var datos= 'ciudad=' + ciudad  + '&tipo=' + tipo + '&precio=' + rango;
     $.ajax({
           type:  "POST",
-          url: "./php/buscador.php",
-          data: filtro,
-          dataType: "json",
+          url: "php/buscador.php",
+          data: datos,
+          type: "json",
           success:  function (response) {
             if(response!=""){
               $(".colContenido").html(response);
@@ -16,5 +23,49 @@ function envio_de_datos(){
             }
           }
     });
-return false;
+  return false;
 }
+
+function datos(){
+    $.ajax({
+          url: "php/lib.php",
+          type: "POST",
+          dataType: "json",
+          success:  function (response) {
+            tituloContenido=response.tituloContenido;
+            ciu = response.ciudades;
+            tip= response.tipos;
+            filtros_I(ciu,tip);
+            for(i=0; i<100; i++){
+              items[i] = response[i];
+            }
+          }
+    });
+}
+
+function filtros_I(ci,ti){
+  var filtro_1 = JSON.stringify(ci);
+  var filtro_2 = JSON.stringify(ti);
+  filtro_1 = filtro_1.substr(1, (filtro_1.length-2));
+  filtro_2 = filtro_2.substr(1, (filtro_2.length-2));
+  filtro_1 = filtro_1.replace(/['"]+/g, '');
+  filtro_2 = filtro_2.replace(/['"]+/g, '');
+  filtro_1 = filtro_1.split(',');
+  filtro_2 = filtro_2.split(',');
+    $('#selectCiudad').append('<option value="' + filtro_1[0] + '">' + filtro_1[0] + '</option>',
+      '<option value="' + filtro_1[1] + '">' + filtro_1[1] + '</option>',
+      '<option value="' + filtro_1[2] + '">' + filtro_1[2] + '</option>',
+      '<option value="' + filtro_1[3] + '">' + filtro_1[3] + '</option>',
+      '<option value="' + filtro_1[4] + '">' + filtro_1[4] + '</option>'
+    );
+    $('#selectTipo').append('<option value="' + filtro_2[0] + '">' + filtro_2[0] + '</option>',
+      '<option value="' + filtro_2[1] + '">' + filtro_2[1] + '</option>',
+      '<option value="' + filtro_2[2] + '">' + filtro_2[2] + '</option>'
+    );
+    $("select").material_select('update');
+}
+
+$(function(){
+  datos();
+  $("#mostrarTodos").click(mostrarTodos);
+})
